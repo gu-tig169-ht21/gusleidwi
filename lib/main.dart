75ad93732c9428d426a35fcedde3063784a7c6a4
wleiding <willeleiding@gmail.com>
2021-11-17 14:48:42 +0100

    steg 2 first
    första commit, ej fungerade checkboxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,136 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(TodoApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Todo {
+  Todo({required this.name, required this.checked});
+  final String name;
+  bool checked;
+}
+
+class TodoItem extends StatelessWidget {
+  TodoItem({
+    required this.todo,
+    required this.onTodoChanged,
+  }) : super(key: ObjectKey(todo));
+
+  final Todo todo;
+  final onTodoChanged;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return TextStyle(
+      color: Colors.black45,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  bool _checked = false;
 
   @override
   Widget build(BuildContext context) {
-    const title = 'TIG169 TODO:';
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(fontFamily: 'RobotoCondensed'),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(
-            Icons.account_box_rounded,
-            size: 35,
-          ),
-          title: const Text(
-            title,
-            style: TextStyle(fontSize: 30),
-          ),
-          backgroundColor: Colors.deepPurple,
-        ),
-        body: ListView(
-          children: const <Widget>[
-            ListTile(
-                title: Text(
-                  'Träna',
-                  style: TextStyle(fontSize: 30),
-                ),
-                leading: Icon(
-                  Icons.check,
-                  size: 35,
-                )),
-            ListTile(
-                leading: Icon(
-                  Icons.check,
-                  size: 35,
-                ),
-                title: Text(
-                  'Plugga',
-                  style: TextStyle(fontSize: 30),
-                )),
-            ListTile(
-                title: Text(
-                  'Städa',
-                  style: TextStyle(fontSize: 30),
-                ),
-                leading: Icon(
-                  Icons.check,
-                  size: 35,
-                )),
-            ListTile(
-                title: Text(
-                  'Slå hål i väggen',
-                  style: TextStyle(fontSize: 30),
-                ),
-                leading: Icon(
-                  Icons.check,
-                  size: 35,
-                )),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-            elevation: 0.0,
-            child: const Icon(Icons.add_sharp),
-            backgroundColor: Colors.deepPurple,
-            onPressed: () {}),
+    return Card(
+        child: CheckboxListTile(
+      title: Text(todo.name, style: _getTextStyle(todo.checked)),
+      value: _checked,
+      onChanged: (newValue) {
+        onTodoChanged(todo);
+        setState(() {
+          _checked = newValue!;
+        });
+      },
+      controlAffinity: ListTileControlAffinity.trailing,
+    ));
+  }
+
+  void setState(Null Function() param0) {}
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final TextEditingController _textFieldController = TextEditingController();
+  final List<Todo> _todos = <Todo>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TIG169 TODO:'),
+        backgroundColor: Colors.orange,
       ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: _todos.map((Todo todo) {
+          return TodoItem(
+            todo: todo,
+            onTodoChanged: _handleTodoChange,
+          );
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(),
+          tooltip: 'Lägg till föremål',
+          backgroundColor: Colors.orange,
+          child: Icon(Icons.add)),
+    );
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.checked = !todo.checked;
+    });
+  }
+
+  void _addTodoItem(String name) {
+    setState(() {
+      _todos.add(Todo(name: name, checked: false));
+    });
+    _textFieldController.clear();
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Lägg till ett nytt föremål i listan'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Vad vill du göra?'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Avbryt'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Lägg till'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: NetworkImage(
-                "https://www.teahub.io/photos/full/8-87200_android-app-background-white.jpg%22")),
-      ),
+    return MaterialApp(
+      title: 'TIG169 TODO:',
+      home: TodoList(),
     );
   }
 }
